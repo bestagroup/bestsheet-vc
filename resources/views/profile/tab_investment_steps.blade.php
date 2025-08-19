f92b026b03fc2f68358d6fb41db553dab60a26a7
hosseindbk <hosseindbk@yahoo.com>
2025-08-19 13:16:36 +0330

    v3.1.15

--- resources/views/profile/tab_investment_steps.blade.php
+++ resources/views/profile/tab_investment_steps.blade.php
@@ -2,7 +2,7 @@
     <div class="mb-4">
         <label class="form-label fw-bold">درصد پیشرفت فرآیند:</label>
         <div class="progress" style="height: 20px;">
-            <div class="progress-bar bg-success" role="progressbar" style="width: {{ round(($projects->invest_step) - 1 / count($investsteps) * 100) }}%;" aria-valuenow="{{ count($investsteps) + 1 }}" aria-valuemin="0" aria-valuemax="{{ count($investsteps) }}">
+            <div class="progress-bar bg-success" role="progressbar" style="width: {{ round(($projects->invest_step)15 / count($investsteps) * 100) }}%;" aria-valuenow="{{ count($investsteps) - 1 }}" aria-valuemin="0" aria-valuemax="{{ count($investsteps) }}">
                 {{ round(($projects->invest_step - 1 ) / count($investsteps) * 100) }}%
             </div>
         </div>
@@ -68,17 +68,142 @@
                                     </div>
                                 </div>
                             @elseif($step->id == 2)
+                                <div class="alert alert-info">در حال بررسی ، لطفا منتظر بمانید...</div>
                             @elseif($step->id == 3)
+                                <div class="alert alert-info">در حال بررسی ، لطفا منتظر بمانید...</div>
                             @elseif($step->id == 4)
+                                <button class="btn btn-sm btn-icon btn-image mx-1 upload-btn" data-id="{{Auth::user()->company->id}}"><i class="mdi mdi-file-document-multiple-outline"></i></button>
+                                <div class="modal fade" id="uploadModal" tabindex="-1" aria-labelledby="uploadModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title"> بارگزاری فایل </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="بستن"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="POST" action="{{ route('storemedia') }}" enctype="multipart/form-data" class="dropzone" id="fileUploadZone" style="min-height: 200px; border-style: dashed; border: 2px dashed #ccc; padding: 20px; margin-bottom: 30px;">
+
+                                                    <input type="hidden" name="record_id" id="recordIdInput">
+                                                    <div class="dz-message text-center text-muted">
+                                                        <div class="mb-3">
+                                                            <i class="bi bi-cloud-arrow-up" style="font-size: 3rem;"></i>
+                                                        </div>
+                                                        <h5 class="fw-bold mb-2">برای آپلود فایل، کلیک کنید یا فایل را بکشید اینجا</h5>
+                                                        <p class="small text-secondary mb-0">فرمت‌های مجاز: JPG, PNG, PDF, MP4, DOCX (حداکثر 10 مگابایت)</p>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @elseif($step->id == 5)
+                                <div class="alert alert-info">در حال بررسی، لطفا منتظر بمانید...</div>
                             @elseif($step->id == 6)
+                                <div class="alert alert-info">1- مجوز ها 2- لیست بیمه تمامی اعضای شرکت 3- مدارک ثبتی 4- مستندات فروش 5- رزومه اعضاء 6- قرارداد فروش 7- قراداد کارکنان 8- نتایج رتبه بندی اعتباری سهامداران 9- اظهارنامه مالیاتی 10- صورت مالی حسابرسی شده شرکت</div>
+                                <button class="btn btn-sm btn-icon btn-image mx-1 upload-btn" data-id="{{Auth::user()->company->id}}"><i class="mdi mdi-file-document-multiple-outline"></i></button>
+                                <div class="modal fade" id="uploadModal" tabindex="-1" aria-labelledby="uploadModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title"> بارگزاری فایل </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="بستن"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="POST" action="{{ route('storemedia') }}" enctype="multipart/form-data" class="dropzone" id="fileUploadZone" style="min-height: 200px; border-style: dashed; border: 2px dashed #ccc; padding: 20px; margin-bottom: 30px;">
+
+                                                    <input type="hidden" name="record_id" id="recordIdInput">
+                                                    <div class="dz-message text-center text-muted">
+                                                        <div class="mb-3">
+                                                            <i class="bi bi-cloud-arrow-up" style="font-size: 3rem;"></i>
+                                                        </div>
+                                                        <h5 class="fw-bold mb-2">برای آپلود فایل، کلیک کنید یا فایل را بکشید اینجا</h5>
+                                                        <p class="small text-secondary mb-0">فرمت‌های مجاز: JPG, PNG, PDF, MP4, DOCX (حداکثر 10 مگابایت)</p>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @elseif($step->id == 7)
+                                <div class="alert alert-info">در حال بررسی، لطفا منتظر بمانید...</div>
                             @elseif($step->id == 8)
+                                <div class="alert alert-info">در حال بررسی، لطفا منتظر بمانید...</div>
                             @elseif($step->id == 9)
+                                <button class="btn btn-sm btn-icon btn-image mx-1 upload-btn" data-id="{{Auth::user()->company->id}}"><i class="mdi mdi-file-document-multiple-outline"></i></button>
+                                <div class="modal fade" id="uploadModal" tabindex="-1" aria-labelledby="uploadModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title"> بارگزاری فایل </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="بستن"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="POST" action="{{ route('storemedia') }}" enctype="multipart/form-data" class="dropzone" id="fileUploadZone" style="min-height: 200px; border-style: dashed; border: 2px dashed #ccc; padding: 20px; margin-bottom: 30px;">
+
+                                                    <input type="hidden" name="record_id" id="recordIdInput">
+                                                    <div class="dz-message text-center text-muted">
+                                                        <div class="mb-3">
+                                                            <i class="bi bi-cloud-arrow-up" style="font-size: 3rem;"></i>
+                                                        </div>
+                                                        <h5 class="fw-bold mb-2">برای آپلود فایل، کلیک کنید یا فایل را بکشید اینجا</h5>
+                                                        <p class="small text-secondary mb-0">فرمت‌های مجاز: JPG, PNG, PDF, MP4, DOCX (حداکثر 10 مگابایت)</p>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @elseif($step->id == 10)
+                                <div class="alert alert-info">در حال بررسی، لطفا منتظر بمانید...</div>
                             @elseif($step->id == 11)
+                                <button class="btn btn-sm btn-icon btn-image mx-1 upload-btn" data-id="{{Auth::user()->company->id}}"><i class="mdi mdi-file-document-multiple-outline"></i></button>
+                                <div class="modal fade" id="uploadModal" tabindex="-1" aria-labelledby="uploadModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title"> بارگزاری فایل </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="بستن"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="POST" action="{{ route('storemedia') }}" enctype="multipart/form-data" class="dropzone" id="fileUploadZone" style="min-height: 200px; border-style: dashed; border: 2px dashed #ccc; padding: 20px; margin-bottom: 30px;">
+
+                                                    <input type="hidden" name="record_id" id="recordIdInput">
+                                                    <div class="dz-message text-center text-muted">
+                                                        <div class="mb-3">
+                                                            <i class="bi bi-cloud-arrow-up" style="font-size: 3rem;"></i>
+                                                        </div>
+                                                        <h5 class="fw-bold mb-2">برای آپلود فایل، کلیک کنید یا فایل را بکشید اینجا</h5>
+                                                        <p class="small text-secondary mb-0">فرمت‌های مجاز: JPG, PNG, PDF, MP4, DOCX (حداکثر 10 مگابایت)</p>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @elseif($step->id == 12)
+                                <div class="alert alert-info">در حال بررسی، لطفا منتظر بمانید...</div>
                             @elseif($step->id == 13)
+                                <h6 class="fw-bold mb-3">قرارداد نهایی</h6>
+                                <div class="table-responsive">
+                                    <table class="table align-middle mb-0">
+                                        <thead class="table-light">
+                                        <tr>
+                                            <th>عنوان قرارداد</th>
+                                            <th>شماره قرارداد</th>
+                                            <th>تاریخ عقد</th>
+                                            <th class="text-center" style="width:90px">فایل</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>قرارداد سرمایه گذاری {{$company->company_name}} </td>
+                                                <td>33556644</td>
+                                                <td>1404/03/01</td>
+                                                <td><a href="{{ asset('#') }}" target="_blank" class="btn btn-sm btn-outline-primary"><i class="mdi mdi-eye"></i></a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             @elseif($step->id == 14)
                             @elseif($step->id == 15)
                             @elseif($step->id == 16)
