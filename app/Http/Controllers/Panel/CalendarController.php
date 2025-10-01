f03ebf72b1b12d164d74caa567371cb7f4f8e1a6
ehsankz <ehsannn321@gmail.com>
2025-10-01 11:08:48 +0330

    v3.1.86 24

--- app/Http/Controllers/Panel/CalendarController.php
+++ app/Http/Controllers/Panel/CalendarController.php
@@ -75,8 +75,8 @@ class CalendarController extends Controller
 
         $googleCalendar = GoogleService::getClient(auth()->user());
 dd( [
-         jdate($request->eventStartDate)->toCarbon(),
-         jdate($request->eventEndDate)->toCarbon()
+         jdate($request->eventStartDate)->format('Y-m-d\TH:i:s')->toCarbon(),
+         jdate($request->eventEndDate)->format('Y-m-d\TH:i:s')->toCarbon()
     ]);
         $event = new Event([
             'summary' => $request->eventTitle ?? 'Test Event',
