73d237274d3c7947045197a397c9f22c6db8835c
ehsankz <ehsannn321@gmail.com>
2025-10-01 09:48:47 +0330

    v3.1.86 9

--- app/Services/GoogleService.php
+++ app/Services/GoogleService.php
@@ -10,10 +10,10 @@ class GoogleService
     public static function getClient($user)
     {
         $client = new Client();
-        $client->setApplicationName('bestsheetweb');
+        $client->setApplicationName('Bestsheet Web App');
         $client->setScopes(Calendar::CALENDAR);
         $client->setAccessType('offline');
-
+dd($user);
         $client->setAccessToken([
             'access_token'  => $user->google_token,
             'expires_in'    => $user->google_expires_in,
