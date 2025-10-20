e5269235ed59ebf73b3d5223d5005bab0810d6cb
ehsankz <ehsannn321@gmail.com>
2025-10-20 10:07:10 +0330

    v3.2.7

--- resources/views/profile/nav_tabs.blade.php
+++ resources/views/profile/nav_tabs.blade.php
@@ -33,7 +33,7 @@
             </button>
         </li>
         <li class="nav-item">
-            <button type="button" {{$project->invest_step > 0 ? '' : 'disabled'}} class="nav-link d-flex flex-column gap-1" role="tab"
+            <button type="button" {{$project->invest_step > 1 ? '' : 'disabled'}} class="nav-link d-flex flex-column gap-1" role="tab"
                     data-bs-toggle="tab" data-bs-target="#navs-minutes-card"
                     aria-controls="navs-minutes-card" aria-selected="false">
                 <i class="tf-icons mdi mdi-text-box-multiple mdi-20px me-1"></i>
@@ -41,7 +41,7 @@
             </button>
         </li>
         <li class="nav-item">
-            <button type="button" {{$project->invest_step > 0 ? '' : 'disabled'}}  class="nav-link d-flex flex-column gap-1" role="tab"
+            <button type="button" {{$project->invest_step > 1 ? '' : 'disabled'}}  class="nav-link d-flex flex-column gap-1" role="tab"
                     data-bs-toggle="tab" data-bs-target="#navs-guarantee-card"
                     aria-controls="navs-guarantee-card" aria-selected="false">
                 <i class="tf-icons mdi mdi-comment-text-multiple mdi-20px me-1"></i>
@@ -49,7 +49,7 @@
             </button>
         </li>
         <li class="nav-item">
-            <button type="button" {{$project->invest_step > 0 ? '' : 'disabled'}}  class="nav-link d-flex flex-column gap-1" role="tab"
+            <button type="button" {{$project->invest_step > 1 ? '' : 'disabled'}}  class="nav-link d-flex flex-column gap-1" role="tab"
                     data-bs-toggle="tab" data-bs-target="#navs-sales-card"
                     aria-controls="navs-guarantee-card" aria-selected="false">
                 <i class="tf-icons mdi mdi-finance mdi-20px me-1"></i>
@@ -57,7 +57,7 @@
             </button>
         </li>
         <li class="nav-item">
-            <button type="button" {{$project->invest_step > 0 ? '' : 'disabled'}}  class="nav-link d-flex flex-column gap-1" role="tab"
+            <button type="button" {{$project->invest_step > 1 ? '' : 'disabled'}}  class="nav-link d-flex flex-column gap-1" role="tab"
                     data-bs-toggle="tab" data-bs-target="#navs-contracts-card"
                     aria-controls="navs-guarantee-card" aria-selected="false">
                 <i class="tf-icons mdi mdi-file-sign mdi-20px me-1"></i>
