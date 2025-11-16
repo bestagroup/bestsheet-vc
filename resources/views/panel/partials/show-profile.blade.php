16a975dff09d4d81684702caccda015fdddfb1d0
ehsankz <ehsannn321@gmail.com>
2025-11-16 15:05:13 +0330

    v3.2.29

--- resources/views/panel/partials/show-profile.blade.php
+++ resources/views/panel/partials/show-profile.blade.php
@@ -441,7 +441,7 @@
                                             </form>
                                     @elseif($step->id == 10)
                                         @foreach($files as $file)
-                                            @if(in_array($file->subject_id, [27,34]) && $file->project_id == $project->id)
+                                            @if(in_array($file->subject_id, [27,30,34]) && $file->project_id == $project->id)
                                                 <div class="alert alert-info record-box" id="record-{{ $file->id }}"> فایل <a href="{{asset('storage/' . $file->file_path)}}" target="_blank"> {{$file->original_name}} </a> در تاریخ {{jdate($file->created_at)->format('d-m-Y')}} بارگزاری شده -</div>
                                             @endif
                                         @endforeach
