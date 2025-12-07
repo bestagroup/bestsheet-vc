22a09a204ad40c21773cea85c58aec44b1578026
ehsankz <ehsannn321@gmail.com>
2025-12-07 16:12:36 +0330

    v3.2.51 v4 2

--- resources/views/panel/partials/show-profile.blade.php
+++ resources/views/panel/partials/show-profile.blade.php
@@ -702,6 +702,7 @@
                                             @endif
                                         @endforeach
                                             <button class="btn btn-md btn-image mx-1 upload-btn" style="min-width: 170px;margin: 30px auto;" data-id="{{$project->id}}" data-subject="24" data-title="مستندات شاخص کلیدی چهارم و سایر"><i class="mdi mdi-file-document-multiple-outline"></i>مستندات شاخص کلیدی چهارم و سایر</button>
+                                            <button class="btn btn-md btn-image mx-1 upload-btn" style="min-width: 170px;margin: 30px auto;" data-id="{{$project->id}}" data-subject="18" data-title="صورتجلسات"><i class="mdi mdi-file-document-multiple-outline"></i>صورتجلسات</button>
 
                                             <form action="{{ route('flow.store') }}" method="POST" class="flow-form d-inline">
                                                 @csrf
